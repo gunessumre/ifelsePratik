85ad26bf7b45b18c601ce25fd1448ad95284bef3
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-10-06 13:26:18 +0300

    Initial commit

---
+++ ifelsePratik1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*if else kullanarak kullanicidan aldigimiz sayinin 10 dan buyuk ya da kucuk mu oldugunu ve
+ iki ye bolunup bolunmedigini yazicaz */ 
+
+int sayi;
+Console.WriteLine("Bir sayi giriniz:");
+sayi = int.Parse(Console.ReadLine());
+if (sayi > 10)
+{
+    Console.WriteLine("Sayı 10'dan büyüktür.");
+}
+else
+{
+    Console.WriteLine("Sayı 10'dan küçüktür.");
+}
+// mod  operatorumuz % isaretiyle mod aliyoruz
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Sayı 2'ye tam bölünür.");
+}
+else
+{
+    Console.WriteLine("Sayı 2'ye tam bölünmez.");
+}
---
+++ ifelsePratik1/ReadME.md
@@ -0,0 +1,3 @@
+# IF Else 
+
+- If-Else yapisini kullanarak kullanicidan aldigimiz sayinin 10 sayisindan buyuk mu kucuk mu oldugunu konsola yazdirdik. 
